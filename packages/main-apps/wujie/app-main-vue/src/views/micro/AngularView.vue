91da897e7730604f96e7a4f7701c090621e1ce94
ningth <ningth_jn@163.com>
2023-03-13 00:20:09 +0800

    feat(angular): init sub app

---
+++ packages/main-apps/wujie/app-main-vue/src/views/micro/AngularView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const url = 'http://localhost:6002/'
+</script>
+
+<template>
+  <WujieVue
+      width="100%"
+      height="100%"
+      name="vite-angular"
+      :url="url"
+  ></WujieVue>
+</template>
